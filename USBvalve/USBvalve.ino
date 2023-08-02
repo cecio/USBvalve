adaf489a823fcd5dbf0466c569e5188087b062ac
cecio <b38911@gmail.com>
2023-08-02 17:20:27 +0200

    bump to release 0.12.2

--- USBvalve/USBvalve.ino
+++ USBvalve/USBvalve.ino
@@ -74,7 +74,7 @@ bool activeState = false;
 //
 // USBvalve globals
 //
-#define VERSION "USBvalve - 0.12.1"
+#define VERSION "USBvalve - 0.12.2"
 boolean readme = false;
 boolean autorun = false;
 boolean written = false;
