8b3f6d3223aa78b24d2c841b1ab68cdeaa345f0c
cecio <b38911@gmail.com>
2023-11-15 21:28:13 +0100

    bump to release 0.14.1

--- USBvalve/USBvalve.ino
+++ USBvalve/USBvalve.ino
@@ -75,7 +75,7 @@ bool activeState = false;
 //
 // USBvalve globals
 //
-#define VERSION "USBvalve - 0.14.0"
+#define VERSION "USBvalve - 0.14.1"
 boolean readme = false;
 boolean autorun = false;
 boolean written = false;
